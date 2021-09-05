b3038918a4e0846bdc3039e40ccac96c577edc55
CodePrometheus <Code_Prince@outlook.com>
2021-09-05 22:29:15 +0800

    fix: 线程池

--- starry-framework/src/main/java/com/star/framework/serialization/impl/KryoThreadLocalConfig.java
+++ starry-framework/src/main/java/com/star/framework/serialization/impl/KryoThreadLocalConfig.java
@@ -1,10 +1,14 @@
 package com.star.framework.serialization.impl;
 
-import java.util.Arrays;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.RejectedExecutionHandler;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Author: zzStar
@@ -12,6 +16,8 @@ import java.util.concurrent.ThreadPoolExecutor;
  * @Description:
  */
 public class KryoThreadLocalConfig implements RejectedExecutionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(KryoThreadLocalConfig.class);
 
     /**
      * 定义一个拒绝列表
@@ -34,11 +40,29 @@ public class KryoThreadLocalConfig implements RejectedExecutionHandler {
      *
      * @param r
      * @param executor
-     */
+     *//*
     @Override
     public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
         Arrays.stream(handlerChain).forEach(rejectedExecutionHandler
                 -> rejectedExecutionHandler.rejectedExecution(r, executor));
+    }*/
+    @Override
+    public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+        try {
+            // 尝试向阻塞队列添加
+            if (!executor.getQueue().offer(r, 60, TimeUnit.SECONDS)) {
+                throw new RejectedExecutionException("Timed Out while attempting to enqueue Task.");
+            }
+        } catch (Exception e) {
+            String msg = String.format("Thread pool is EXHAUSTED!" +
+                            " Pool Size: %d (active: %d, core: %d, max: %d, largest: %d), Task: %d (completed: %d)," +
+                            " Executor status:(isShutdown:%s, isTerminated:%s, isTerminating:%s)",
+                    executor.getPoolSize(), executor.getActiveCount(), executor.getCorePoolSize(), executor.getMaximumPoolSize(), executor.getLargestPoolSize(),
+                    executor.getTaskCount(), executor.getCompletedTaskCount()
+                    , executor.isShutdown(), executor.isTerminated(), executor.isTerminating());
+            log.warn(msg);
+            throw new RejectedExecutionException(msg);
+        }
     }
 
 }
